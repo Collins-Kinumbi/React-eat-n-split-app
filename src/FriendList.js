1131fb683b7098d44747f2234837d9e472303499
Collins Kinumbi <kinumbicollins254@gmail.com>
2024-02-20 18:16:39 +0300

    Initial commit

---
+++ src/FriendList.js
@@ -0,0 +1,20 @@
+import Friend from "./Friend";
+
+function FriendsList({ friends, onSelection, selectedFriend }) {
+  // const friends = initialFriends;
+
+  return (
+    <ul>
+      {friends.map((friend) => (
+        <Friend
+          friend={friend}
+          key={friend.id}
+          onSelection={onSelection}
+          selectedFriend={selectedFriend}
+        />
+      ))}
+    </ul>
+  );
+}
+
+export default FriendsList;
